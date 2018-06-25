7f8a238a582cf740b8cc83e332f9d98fa9cb9381
Isuranga Perera <isurangamperera@gmail.com>
2018-06-25 20:36:13 +0530

    BAH-560: add database data to liquibase

--- omod/src/main/java/org/openmrs/module/notifications/rest/resource/SubscriptionResource.java
+++ omod/src/main/java/org/openmrs/module/notifications/rest/resource/SubscriptionResource.java
@@ -18,11 +18,10 @@ import org.openmrs.module.webservices.rest.web.response.ResponseException;
 
 import java.util.List;
 
-@Resource(name = RestConstants.VERSION_1
-		+ "/notification", supportedClass = Subscription.class, supportedOpenmrsVersions = {"1.9.*", "1.10.*", "1.11.*",
-				"1.12.*", "2.0.*", "2.1.*"})
+@Resource(name = RestConstants.VERSION_1 + "/notification", supportedClass = Subscription.class, supportedOpenmrsVersions = {
+        "1.9.*", "1.10.*", "1.11.*", "1.12.*", "2.0.*", "2.1.*" })
 public class SubscriptionResource extends DelegatingCrudResource<Subscription> {
-
+	
 	private DelegatingResourceDescription getDescription() {
 		DelegatingResourceDescription description = new DelegatingResourceDescription();
 		description.addProperty("id");
@@ -33,7 +32,7 @@ public class SubscriptionResource extends DelegatingCrudResource<Subscription> {
 		description.addProperty("user");
 		return description;
 	}
-
+	
 	@Override
 	public DelegatingResourceDescription getRepresentationDescription(Representation rep) {
 		if ((rep instanceof DefaultRepresentation) || (rep instanceof RefRepresentation)) {
@@ -44,36 +43,35 @@ public class SubscriptionResource extends DelegatingCrudResource<Subscription> {
 		}
 		return null;
 	}
-
+	
 	@Override
 	protected PageableResult doGetAll(RequestContext context) throws ResponseException {
-		List<Subscription> subscriptionList = Context.getService(SubscriptionManagementService.class)
-				.getAllSubscriptions();
+		List<Subscription> subscriptionList = Context.getService(SubscriptionManagementService.class).getAllSubscriptions();
 		return new AlreadyPaged<Subscription>(context, subscriptionList, false);
 	}
-
+	
 	@Override
 	public Subscription getByUniqueId(String s) {
 		return null;
 	}
-
+	
 	@Override
 	protected void delete(Subscription subscription, String s, RequestContext requestContext) throws ResponseException {
-
+		
 	}
-
+	
 	@Override
 	public Subscription newDelegate() {
 		return null;
 	}
-
+	
 	@Override
 	public Subscription save(Subscription subscription) {
 		return Context.getService(SubscriptionManagementService.class).saveSubscription(subscription);
 	}
-
+	
 	@Override
 	public void purge(Subscription subscription, RequestContext requestContext) throws ResponseException {
-
+		
 	}
 }
